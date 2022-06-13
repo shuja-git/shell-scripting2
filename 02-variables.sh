af8edb0f3bd68696adaa570b88df8b7fa7640a86
shuja <shuja.devops@gmail.com>
2022-06-13 15:14:16 +0530

    reading usr input

--- 02-variables.sh
+++ 02-variables.sh
@@ -9,3 +9,7 @@ echo 'Addition of two numbers is' , ${addition}
 
 echo USER=${USER}
 echo A=${A}
+
+read -p 'enter ur age ' age
+echo -e "\e[34mYour age is ${age} but dont worry age is just the \e[1;32mNumber\e0m"
+
