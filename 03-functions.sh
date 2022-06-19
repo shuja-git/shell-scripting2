55e6cf1932d7340531bebd80336308cf8c373507
shuja <shuja.devops@gmail.com>
2022-06-19 23:08:33 +0530

    space fix

--- 03-functions.sh
+++ 03-functions.sh
@@ -16,8 +16,8 @@ sample2(){
   echo first argument = $1
   echo Numbr of arguments = $#
   echo value of a in function = ${a}
-  b = 200
+  b=200
 }
-a = 100
+a=100
 sample2 123 abc
 echo value of b = ${b}
