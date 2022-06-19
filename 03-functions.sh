0a68666b6ae35185878c00d8d544304f60b9993b
shuja <shuja.devops@gmail.com>
2022-06-19 23:07:12 +0530

    declared var in function and used in main program and vice-vrsa

--- 03-functions.sh
+++ 03-functions.sh
@@ -15,6 +15,9 @@ sample1
 sample2(){
   echo first argument = $1
   echo Numbr of arguments = $#
-
+  echo value of a in function = ${a}
+  b = 200
 }
+a = 100
 sample2 123 abc
+echo value of b = ${b}
