0d0b542c059eabf6b6e7e914b7bc344e0248168d
shuja <shuja.devops@gmail.com>
2022-06-19 22:31:30 +0530

    -f option in curl added

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -30,7 +30,7 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
+curl  -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
 if [ $? -ne 0 ]; then
   echo "Download frontend failed"
   exit
