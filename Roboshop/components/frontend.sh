df7525b9d04c6dd015bb0713953b8db69a4cb075
shuja <shuja.devops@gmail.com>
2022-06-19 22:18:53 +0530

    removed p char for testing

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -22,7 +22,7 @@ echo frontend setup
 
 yum install nginx -y
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
