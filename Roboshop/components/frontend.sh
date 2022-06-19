68e4ae2ff3c2bb43224be773ecffa6cecc4e6e77
shuja <shuja.devops@gmail.com>
2022-06-19 23:25:55 +0530

    0m fix

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -24,10 +24,10 @@
 
 STAT_CHECK(){
 if [ $1 -ne 0 ]; then
-  echo -e "\e[1;31m${2}- Failed\e0m"
+  echo -e "\e[1;31m${2} - Failed\e[0m"
   exit 1
 else
-  echo -e "\e[1;31m${2}- Success"
+  echo -e "\e[1;32m${2}- Success\e[0m"
 fi
 
 }
