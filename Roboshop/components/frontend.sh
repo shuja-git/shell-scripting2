ae382846289f509b413f046e97f6608a012bb7fd
shuja <shuja.devops@gmail.com>
2022-06-20 23:08:08 +0530

    frontend all code

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -39,13 +39,24 @@ yum install nginx -y >>${LOG_FILE}
 STAT_CHECK $? "Nginx install"
 curl  -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
 STAT_CHECK $? "Download nginx"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+#rm -rf *
+STAT_CHECK $? "Remove Old HTML pages"
+
+#unzip /tmp/frontend.zip
+#mv frontend-main/* .
+#mv static/* .
+cd /tmp && unzip -o /tmp/frontend.zip &>>${LOG_FILE}
+STAT_CHECK $? "Extracting frontend content"
+
+#rm -rf frontend-master static README.md
+
+cd /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
+STAT_CHECK $? "copying frontend content"
+
+#mv localhost.conf /etc/nginx/default.d/roboshop.conf
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $? "Update Nginx config file"
+systemctl enable nginx &>>${LOG_FILE }&& systemctl restart nginx
+STAT_CHECK $? "Restart Nginx"
 
